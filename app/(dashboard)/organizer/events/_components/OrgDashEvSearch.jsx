0233c5157ee3e4f5e345990af00247afa4ccc0d3
TRISHAN00 <trishansaha43@gmail.com>
2026-05-21 20:04:40 +0600

    Add: Search and Tab

---
+++ app/(dashboard)/organizer/events/_components/OrgDashEvSearch.jsx
@@ -0,0 +1,17 @@
+import { Input } from "@/app/components/ui/input";
+import { Search } from "lucide-react";
+
+export default function OrgDashEvSearch({search, setSearch}) {
+
+  return (
+    <div className="relative flex-1 max-w-md">
+      <Search className="absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2 text-muted-foreground" />
+      <Input 
+        placeholder="Search events..." 
+        className="pl-10" 
+        value={search} 
+        onChange={(e) => setSearch(e.target.value)} 
+      />
+    </div>
+  );
+}
--- app/(dashboard)/organizer/events/page.js
+++ app/(dashboard)/organizer/events/page.js
@@ -2,9 +2,13 @@
 
 import DashboardEventFooter from "@/app/(admin)/dashboard/events/_components/DashboardEventFooter";
 import DashboardEventLoading from "@/app/(admin)/dashboard/events/_components/DashboardEventLoading";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 import { useCallback, useEffect, useState } from "react";
 import { getAllDashboardOrganizerEvents } from "../services/organizer.event.service";
 import OrgDashEventList from "./_components/OrgDashEventList";
+import OrgDashEvSearch from "./_components/OrgDashEvSearch";
+import OrgDashEvTab from "./_components/OrgDashEvTab";
 
 export default function DashboardOrganizerEventsPage() {
   const [loading, setLoading] = useState(false);
@@ -16,8 +20,6 @@ export default function DashboardOrganizerEventsPage() {
   const [isRunRiseNation, setIsRunRiseNation] = useState(true);
   const [eventType, setEventType] = useState("");
   const [date, setDate] = useState(null);
-
-  console.log("events", events);
 
   const fetchEvents = useCallback(async () => {
     const token =
@@ -54,6 +56,19 @@ export default function DashboardOrganizerEventsPage() {
 
   return (
     <div className="space-y-6 animate-fade-in">
+      <div className="flex flex-col gap-4 sm:flex-row sm:items-center justify-between">
+        <OrgDashEvSearch search={search} setSearch={setSearch} />
+
+        <div className="flex flex-wrap items-center gap-4">
+          <OrgDashEvTab
+            eventType={eventType}
+            setEventType={setEventType}
+          />
+          <Link href={"/organizer/events/create"}>
+            <Button>Create Event</Button>
+          </Link>
+        </div>
+      </div>
 
       {loading ? (
         <DashboardEventLoading />
