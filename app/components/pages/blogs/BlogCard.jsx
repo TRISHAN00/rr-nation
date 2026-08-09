8e072e9db984560b2edf04e2f7745fb84ec9c71f
TRISHAN00 <trishansaha43@gmail.com>
2026-08-10 01:59:21 +0600

    remove the author div from the feature blog section of home page

--- app/components/pages/blogs/BlogCard.jsx
+++ app/components/pages/blogs/BlogCard.jsx
@@ -1,4 +1,3 @@
-import { Avatar, AvatarFallback, AvatarImage } from "@/app/components/ui/avatar";
 import Image from "next/image";
 
 export default function BlogCard({ blog }) {
@@ -44,34 +43,6 @@ export default function BlogCard({ blog }) {
           <p className="text-gray-600 text-sm sm:text-base leading-relaxed line-clamp-2">
             {body}
           </p>
-        </div>
-
-        {/* AUTHOR + CTA */}
-        <div className="flex items-center justify-between">
-          <div className="flex items-center gap-3 sm:gap-4">
-            <Avatar className="w-10 h-10 sm:w-12 sm:h-12 border border-[#4DC9C1]">
-              <AvatarImage src={`https://api.dicebear.com/7.x/initials/svg?seed=${author}`} />
-              <AvatarFallback>
-                {author
-                  ?.split(" ")
-                  .map((n) => n[0])
-                  .join("")}
-              </AvatarFallback>
-            </Avatar>
-
-            <h4 className="text-sm sm:text-base font-semibold text-gray-800">
-              {author}
-            </h4>
-          </div>
-
-          {/* <Link
-            href={`/blogs/${blog.id}`}
-            className="flex items-center justify-center w-9 h-9 sm:w-10 sm:h-10 rounded-full border border-brand text-brand transition hover:bg-brand hover:text-white"
-          >
-            <div className="-rotate-45 group-hover:rotate-0 transition-transform duration-300">
-              <ArrowDownRight size={18} />
-            </div>
-          </Link> */}
         </div>
       </div>
     </div>
