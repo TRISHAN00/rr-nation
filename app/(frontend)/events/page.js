95bb33fe70d075208c9d2c8afeb57d86726b0801
TRISHAN00 <trishansaha43@gmail.com>
2026-08-11 19:13:50 +0600

    feat: wire feature-team CMS section into about page and team carousel

--- app/(frontend)/events/page.js
+++ app/(frontend)/events/page.js
@@ -1,15 +1,16 @@
 import InnerBanner from "@/app/components/common/InnerBanner";
 import Events from "@/app/components/pages/events/Events";
+import { getApi } from "../api/page-api";
 
 export const metadata = {
   title: "Upcoming Running Events & Marathons | RunRise Nation",
   description: "Register for the latest marathons, 15K runs, and virtual challenges in Bangladesh. Join RunRise Nation's officially timed events and track your progress.",
   keywords: [
-    "RunRise Nation Events", 
-    "Bangladesh Marathons 2026", 
-    "15K Run Dhaka", 
-    "Virtual Running Challenges", 
-    "AIMS Certified Races Bangladesh", 
+    "RunRise Nation Events",
+    "Bangladesh Marathons 2026",
+    "15K Run Dhaka",
+    "Virtual Running Challenges",
+    "AIMS Certified Races Bangladesh",
     "Running Competition"
   ],
   openGraph: {
@@ -18,7 +19,7 @@ export const metadata = {
     type: "website",
     images: [
       {
-        url: "/dynamic/events-og.jpg", 
+        url: "/dynamic/events-og.jpg",
         width: 1200,
         height: 630,
         alt: "RunRise Nation Race Start Line",
@@ -27,15 +28,22 @@ export const metadata = {
   },
 };
 
-export default function EventsPage() {
+export default async function EventsPage() {
+  const apiEvValue = "events";
+  const eventData = await getApi(apiEvValue);
+
+  const bannerData = eventData?.data?.sections?.find(
+    (f) => f?.section_data?.slug === "event-banner"
+  );
+
   return (
     <>
       <InnerBanner
-        title="Events"
-        background="/dynamic/event/inner-banner.jpg"
+        title={bannerData?.section_data?.subtitle}
+        background={bannerData?.images?.list?.[0]?.full_path}
         breadcrumbs={[{ label: "Home", href: "/" }, { label: "Events" }]}
       />
-      <Events  />
+      <Events />
     </>
   );
 }
